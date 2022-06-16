b5534796b5e6cc45e9eac05a5ef63e0ed548bec4
Nikolay Osolodkov <nnosolodkov@bk.ru>
2022-06-16 18:08:51 +0300

    Add project files and docker-compose to run RabbitMQ in Docker.

---
+++ Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using var channel = connection.CreateModel();
+    channel.QueueDeclare(
+        queue: "dev-queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += Consumer_Received;
+
+    channel.BasicConsume(
+        queue: "dev-queue",
+        autoAck: true,
+        consumer: consumer);
+
+    Console.WriteLine("Consumer subscribed to the dev-queue");
+    Console.ReadLine();
+}
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body;
+    var message = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine("Received message: {0}", message);
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using var channel = connection.CreateModel();
+    channel.QueueDeclare(
+        queue: "dev-queue",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var counter = 0;
+    do
+    {
+        // Delay between sending.
+        Thread.Sleep(Random.Shared.Next(1000, 3000));
+
+        var message = $"Message #{counter} from publisher";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            "dev-queue",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($"The message #{counter} was sent to the default exchange.");
+
+        counter++;
+    } while (true);
+}
